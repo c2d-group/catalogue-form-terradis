fbbe20245eba7d233fcb3707c06aadecc80e80f7
Clément Deryckx <clement.deryckx@outlook.com>
2022-03-24 18:41:30 +0100

    css for desktop is ok, inputs animation is functionnal

--- front/js/app.js
+++ front/js/app.js
@@ -1,3 +1,9 @@
+// TODO : Ne générer que les messages d'erreurs ou il y a une erreur lors de l'envoi.
+
+
+import Validation from './classes/Validation.js';
+const validation = new Validation();
+
 const firstnameInput = document.querySelector('input[name="firstname"]');
 const lastnameInput = document.querySelector('input[name="lastname"]');
 const companyInput = document.querySelector('input[name="company"]');
@@ -6,8 +12,8 @@ const sectorInput = document.querySelector('input[name="sector"]');
 const submitBtn = document.querySelector('#catalogue-form button[type="submit"]');
 const btn = document.querySelector('a[id="form-btn"]');
 const catalogue = document.querySelector('a[id="link-catalogue"]');
-const finput = document.querySelector('#fill');
-const form = document.querySelector('form[id="catalogue-form"]')
+const fill = document.querySelector('#fill');
+const form = document.querySelector('form[id="catalogue-form"]');
 
 // Remplissage rapide pour test
 fill.addEventListener('click', fillFormsInput);
@@ -20,7 +26,7 @@ function validateForm(){
     let err = [];
     if(firstnameInput.value.length < 2 ){
         if(document.querySelector(`.firstnameGroup .validation-text`)) document.querySelector(`.firstnameGroup .validation-text`).remove();
-        if(!document.querySelector('.firstnameGroup .validation-text')) formError(firstnameInput, 'Merci de renseigner votre nom');
+        if(!document.querySelector('.firstnameGroup .validation-text')) validation.formError(firstnameInput, 'Merci de renseigner votre nom');
         err.push('Merci de renseigner votre nom');
     } 
     else {
@@ -28,7 +34,7 @@ function validateForm(){
      }
     if(lastnameInput.value.length < 2){
         if(document.querySelector(`.lastnameGroup .validation-text`)) document.querySelector(`lastnameGroup .validation-text`).remove();
-        if(!document.querySelector('.lastnameGroup .validation-text')) formError(lastnameInput, 'Merci de renseigner votre prénom');
+        if(!document.querySelector('.lastnameGroup .validation-text')) validation.formError(lastnameInput, 'Merci de renseigner votre prénom');
         err.push('Merci de renseigner votre prénom');
     } 
     else {
@@ -36,15 +42,15 @@ function validateForm(){
      }
     if(companyInput.value.length === 0){
         if(document.querySelector(`.companyGroup .validation-text`)) document.querySelector(`companyGroup .validation-text`).remove();
-        if(!document.querySelector('.companyGroup .validation-text')) formError(companyInput, 'Merci de renseigner votre entreprise');
+        if(!document.querySelector('.companyGroup .validation-text')) validation.formError(companyInput, 'Merci de renseigner votre entreprise');
         err.push('Merci de renseigner votre entreprise');
     } 
     else {
         if(document.querySelector('.companyGroup .validation-text')) document.querySelector('.companyGroup .validation-text').remove();
      }
-    if(validateEmail(mailInput.value) === null ){
+    if(validation.validateEmail(mailInput.value) === null ){
         if(document.querySelector(`.mailGroup .validation-text`)) document.querySelector(`mailGroup .validation-text`).remove();
-        if(!document.querySelector(`.mailGroup .validation-text`)) formError(mailInput, 'Merci de renseigner une adresse mail valide');
+        if(!document.querySelector(`.mailGroup .validation-text`)) validation.formError(mailInput, 'Merci de renseigner une adresse mail valide');
         err.push('Merci de renseigner une adresse mail valide');
     } 
     else {
@@ -52,11 +58,11 @@ function validateForm(){
      }
     if(parseInt(sectorInput.value)  < 0 || parseInt(sectorInput.value) > 99){
         if(document.querySelector(`.sectorGroup .validation-text`)) document.querySelector(`sectorGroup .validation-text`).remove();
-        if(!document.querySelector(`.sectorGroup .validation-text`)) formError(sectorInput, 'Merci de renseigner un département valide');
+        if(!document.querySelector(`.sectorGroup .validation-text`)) validation.formError(sectorInput, 'Merci de renseigner un département valide');
         err.push('Merci de renseigner un département valide');
     } else if (sectorInput.value.length === 0) { 
         if(document.querySelector(`.sectorGroup .validation-text`)) document.querySelector(`sectorGroup .validation-text`).remove();
-        if(!document.querySelector(`.sectorGroup .validation-text`)) formError(sectorInput, 'Merci de renseigner votre département');
+        if(!document.querySelector(`.sectorGroup .validation-text`)) validation.formError(sectorInput, 'Merci de renseigner votre département');
         err.push('Merci de renseigner votre département');
      } else {
         if(document.querySelector('.sectorGroup .validation-text')) document.querySelector('.sectorGroup .validation-text').remove();
@@ -73,33 +79,29 @@ function validateForm(){
     }
 }
 
-function formError(input, errorValue){
-    const validation = document.createElement('span');
-    validation.className = "validation-text";
-    validation.innerText = errorValue;
-    input.parentElement.appendChild(validation); 
-    
-    // Delete on ui dev
-    validation.style.color = "red";
+const inputs = document.querySelectorAll('input');
+const inputsTexts = document.querySelectorAll('.form-input .input-text');
+const formGroups = document.querySelectorAll('.form-group');
 
+for(let i = 0; i < inputs.length; i++){
+    inputs[i].addEventListener('input', (e) => {
+        inputsTexts[i].innerText = inputs[i].value;
+        console.log(inputsTexts[i]);
+        console.log(inputsTexts[i].value);
+    })
 }
 
-function validateEmail (email) {
-    return email.match(
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
 
+formGroups.forEach((group) => {
+    group.addEventListener('click', (e) => {
+        group.classList.add('focus');
+    })
+})
 
-// Module de remplisagge express
-function fillInput(input, value){
-    input.innerText = value;
-    input.value = value;
-}
 function fillFormsInput(){
-    fillInput(firstnameInput, 'Deryckx');
-    fillInput(lastnameInput, 'Clément');
-    fillInput(companyInput, 'C2D');
-    fillInput(mailInput, 'deryckxclement@outlook.com');
-    fillInput(sectorInput, 59);
+    validation.fillInput(firstnameInput, 'Deryckx');
+    validation.fillInput(lastnameInput, 'Clément');
+    validation.fillInput(companyInput, 'C2D');
+    validation.fillInput(mailInput, 'deryckxclement@outlook.com');
+    validation.fillInput(sectorInput, 59);
 }
